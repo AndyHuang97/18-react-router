6e04e048a5f9bc9784a6c10c4b4700c3fefdfbd7
Andrea Huang <huangandrea@hotmail.it>
2025-05-06 21:19:21 +0200

    Working with Navigation Links (NavLink)

--- src/components/MainNavigation.jsx
+++ src/components/MainNavigation.jsx
@@ -1,5 +1,7 @@
-import { Link } from "react-router-dom";
-import classes from "./MainNavigation.module.css"
+import { NavLink } from "react-router-dom";
+import classes from "./MainNavigation.module.css";
+
+// Link/NavLink elements return an "a" element in HTML
 
 export default function MainNavigation() {
   return (
@@ -7,10 +9,26 @@ export default function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to='/'>Home</Link>
+            <NavLink
+              to='/'
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end // to tell if the path ends with "/" to be active
+              // style={({ isActive }) => ({textAlign: isActive ? "center" : "left"})}
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to='/products'>Products</Link>
+            <NavLink
+              to='/products'
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
