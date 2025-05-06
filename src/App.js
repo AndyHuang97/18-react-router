497318b52b5b117f2fb80cf0a82a91d313dbf84b
Andrea Huang <huangandrea@hotmail.it>
2025-05-06 21:30:38 +0200

    Defining & Using Dynamic Routes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
+import ProductDetails from "./pages/ProductDetails";
 
 // Alternative (old) way of defining routes
 // const routeDefinitions = createRoutesFromElements(
@@ -26,6 +27,7 @@ const router = createBrowserRouter([
     children: [ // => element acts as wrapper
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
+      { path: "/products/:productId", element: <ProductDetails /> },
     ],
   },
   // {
